33d6442dd471f2f1263c35392e9f24bc9cca0585
Collins Mutai <collinsfrontend@gmail.com>
2025-03-23 17:53:03 +0300

    ui

--- backend/app.js
+++ backend/app.js
@@ -33,10 +33,7 @@ app.use(
 
 // ✅ Global Headers Middleware
 app.use((req, res, next) => {
-  res.header(
-    "Access-Control-Allow-Origin",
-    "https://jupscarwash.onrender.com"
-  );
+  res.header("Access-Control-Allow-Origin", "https://jupscarwash.onrender.com");
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
   res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
   res.header("Access-Control-Allow-Credentials", "true");
